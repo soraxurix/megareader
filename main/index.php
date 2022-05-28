1186c9fd147e68373d6a349c9cd67d1bb1054bca
soraxurix <71204059+soraxurix@users.noreply.github.com>
2022-05-28 08:15:40 -0500

    preuba nombre

--- main/index.php
+++ main/index.php
@@ -39,10 +39,17 @@
 </head>
 <body>
 	<?php 
-		include 'sesion/comprobar_sesion.php'; 
-		
-		echo "id: ".$usuario;
-		echo "<br> nombre: ".$nombre;
+		session_start();
+		$usuario = $_SESSION['id_user'];
+		$nombre = $_SESSION['name_user'];
+		echo "<h1>".$nombre."</h1>";
+
+		if(!isset($usuario)){
+			echo "No se cargaron los datos del usuario<br>";
+		}else{
+			echo "Se cargaron los datos del usuario<br>";
+		}
+
 
 	?>
 
--- main/sesion/comprobar_sesion.php
+++ main/sesion/comprobar_sesion.php
@@ -1,14 +1,4 @@
 <?php 
-session_start();
-$usuario = $_SESSION['id_user'];
-$nombre = $_SESSION['name_user'];
-echo "<h1>".$nombre."</h1>";
-
-if(!isset($usuario)){
-	echo "No se cargaron los datos del usuario<br>";
-}else{
-	echo "Se cargaron los datos del usuario<br>";
-}
 
 
  ?>
