cbac26c831a54c584faba8061dced43f7434f669
soraxurix <71204059+soraxurix@users.noreply.github.com>
2022-05-28 09:05:01 -0500

    preuba nombre

--- main/index.php
+++ main/index.php
@@ -1,5 +1,15 @@
 <?php 
-	include 'sesion/cerrar_sesion.php';
+	session_start();
+	$usuario = $_SESSION['id_user'];
+	$nombre = $_SESSION['name_user'];
+	
+	if(!isset($usuario)){
+		header("location: https://megareader.azurewebsites.net/");
+		die();
+	}else{
+		echo "Se cargaron los datos del usuario<br>";
+	}
+	echo "<h1>Nombre: ".$nombre."</h1>";
 ?>
 
 <!DOCTYPE html>
