73f83ffd2f26559aeccb096f129fecffae853cf0
soraxurix <71204059+soraxurix@users.noreply.github.com>
2022-05-28 12:23:21 -0500

    agregando registro

--- signin/crear_cliente.php
+++ signin/crear_cliente.php
@@ -5,6 +5,16 @@ session_start();
 include '../clases/conexion.php';
 include '../clases/encriptacion.php';
 
-echo $_POST['nombre'];
+$name = $_POST['nombre'];	
+$email = $_POST['email'];
+$pass = $_POST['pass'];	
+
+$pass_ecrypted = getEncryptedPassword($pass); 
+
+$consulta = "INSERT into usuarios (username, email, pass, fecha_registro) values ('$name', '$email', '$pass_ecrypted', now());";
+// $result = mysqli_query($conexion, $consulta);	
+echo $consulta;
+
+
 
  ?>
