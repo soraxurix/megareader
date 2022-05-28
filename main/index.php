6cfa8989f100537e0daab8b4d0098fa44d3cc3f6
soraxurix <71204059+soraxurix@users.noreply.github.com>
2022-05-28 00:04:49 -0500

    agregando navbar 2

--- main/index.php
+++ main/index.php
@@ -48,7 +48,7 @@
 		    		    	  
 		    <div class="navbar-end">
 				<div class="navbar-item has-dropdown is-hoverable">
-					<a class="navbar-link is-size-5 has-text-info" href="#"> <?php $_SESSION['name_user']?> </a>
+					<a class="navbar-link is-size-5 has-text-info" href="#"> <?php echo $_SESSION['name_user']?> </a>
 					<div class="navbar-dropdown is-boxed">
 						<a class="navbar-item is-size-5 icon-text">
 							<span class="icon has-text-success"><i class="fas fa-book"></i></span> 
