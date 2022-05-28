344bd8f238a7faad1848aab09e7faecb1fe2c2d5
soraxurix <71204059+soraxurix@users.noreply.github.com>
2022-05-28 00:08:17 -0500

    agregando navbar 2

--- main/sesion/comprobar_sesion.php
+++ main/sesion/comprobar_sesion.php
@@ -3,7 +3,7 @@ session_start();
 $usuario = $_SESSION['id_user'];
 
 if(!isset($usuario)){
-	header("location: https://megareader.azurewebsites.net/main/");
+	header("location: https://megareader.azurewebsites.net/");
 }
 
 
