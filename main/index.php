ce3c34aafba241ed1d4adea2962a85e9fa684182
soraxurix <71204059+soraxurix@users.noreply.github.com>
2022-05-28 09:01:51 -0500

    preuba nombre

--- main/index.php
+++ main/index.php
@@ -1,15 +1,5 @@
 <?php 
-	session_start();
-	$usuario = $_SESSION['id_user'];
-	$nombre = $_SESSION['name_user'];
-	
-	if(!isset($usuario)){
-		header("location: https://megareader.azurewebsites.net/");
-		die();
-	}else{
-		echo "Se cargaron los datos del usuario<br>";
-	}
-	echo "<h1>Nombre: ".$nombre."</h1>";
+	include 'sesion/cerrar_sesion.php';
 ?>
 
 <!DOCTYPE html>
--- main/sesion/comprobar_sesion.php
+++ main/sesion/comprobar_sesion.php
@@ -1,4 +1,14 @@
 <?php 
-
+	session_start();
+	$usuario = $_SESSION['id_user'];
+	$nombre = $_SESSION['name_user'];
+	
+	if(!isset($usuario)){
+		header("location: https://megareader.azurewebsites.net/");
+		die();
+	}else{
+		echo "Se cargaron los datos del usuario<br>";
+	}
+	echo "<h1>Nombre: ".$nombre."</h1>";
 
  ?>
