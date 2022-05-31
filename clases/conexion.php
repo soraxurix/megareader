8198af9ff2533dbd1fc7052ae084d8fcde9e1089
soraxurix <71204059+soraxurix@users.noreply.github.com>
2022-05-31 15:05:41 -0500

    agregando cadena conecion nuevo

--- clases/conexion.php
+++ clases/conexion.php
@@ -1,18 +1,18 @@
 <?php 
 
 $servidor = "sql10.freemysqlhosting.net";
-$usuario = "sql10494751";
-$password = "6ju1W72rrT";
-$db = "sql10494751";
+$usuario = "sql10496801";
+$password = "Fnj8GYZpdZ";
+$db = "sql10496801";
+
+// $servidor = "sql10.freemysqlhosting.net";
+// $usuario = "sql10494751";
+// $password = "6ju1W72rrT";
+// $db = "sql10494751";
 
 $conexion = mysqli_connect($servidor,$usuario,$password,$db);
-mysqli_set_charset($conexion, 'utf8');
-/*if($conexion){
+/*mysqli_set_charset($conexion, 'utf8');
+if($conexion){
 	echo "Se conecto a la base de datos";
 }*/
-// Server: sql10.freemysqlhosting.net
-// Name: sql10494751
-// Username: sql10494751
-// Password: 6ju1W72rrT
-// Port number: 3306
  ?>
--- clases/encriptacion.php
+++ clases/encriptacion.php
@@ -20,4 +20,5 @@ function getUnencryptedPassword ($encrypted){
 }
 
 
+
  ?>
